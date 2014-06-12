fff05eb8be04bd4cd9aae3be87f7e4d021a8534e
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-06-12 15:28:37 +0100

    Exporter tool suports now all the MAIN resources

--- Core/src/main/java/org/reactome/server/tools/ExporterTool.java
+++ Core/src/main/java/org/reactome/server/tools/ExporterTool.java
@@ -4,7 +4,8 @@ import com.martiansoftware.jsap.*;
 import org.gk.persistence.MySQLAdaptor;
 import org.reactome.server.EntryPoint;
 import org.reactome.server.components.analysis.data.AnalysisData;
-import org.reactome.server.components.exporter.UniProtExporter;
+import org.reactome.server.components.analysis.model.resource.ResourceFactory;
+import org.reactome.server.components.exporter.Exporter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -22,6 +23,8 @@ public class ExporterTool {
                                 "The tool to use. Options: " + EntryPoint.Tool.getOptions()) //WE DO NOT TAKE INTO ACCOUNT TOOL HERE ANY MORE
                         ,new FlaggedOption( "host", JSAP.STRING_PARSER, "localhost", JSAP.NOT_REQUIRED, 'h', "host",
                         "The database host")
+                        ,new FlaggedOption( "resource", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'r', "resource",
+                        "The resource to export data from (Options [ "+ getAvailableResources() + " ]")
                         ,new FlaggedOption( "database", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'd', "database",
                         "The reactome database name to connect to")
                         ,new FlaggedOption( "username", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'u', "username",
@@ -52,7 +55,21 @@ public class ExporterTool {
         AnalysisData analysisData = context.getBean(AnalysisData.class);
         analysisData.setFileName(config.getString("input"));
 
-        UniProtExporter uniProtExporter = context.getBean(UniProtExporter.class);
-        uniProtExporter.export(dba);
+        String resource = config.getString("resource");
+        ResourceFactory.MAIN mainResource = ResourceFactory.getMainResource(resource);
+        if(mainResource!=null){
+            Exporter exporter = context.getBean(Exporter.class);
+            exporter.export(dba, mainResource);
+        }else{
+            System.err.println("Invalid resource " + resource + ". Available options are: " + getAvailableResources());
+        }
+    }
+
+    private static String getAvailableResources(){
+        StringBuilder sb = new StringBuilder();
+        for (ResourceFactory.MAIN main : ResourceFactory.MAIN.values()) {
+            sb.append(" ").append(main.toString());
+        }
+        return sb.toString();
     }
 }
