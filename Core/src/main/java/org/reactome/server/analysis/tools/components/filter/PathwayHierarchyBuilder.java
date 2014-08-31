a3b2ae2c201a04e1613f39db0d18d222e377a186
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-08-31 15:21:45 +0100

    Analysis filter checks for species oddities

--- Core/src/main/java/org/reactome/server/analysis/tools/components/filter/PathwayHierarchyBuilder.java
+++ Core/src/main/java/org/reactome/server/analysis/tools/components/filter/PathwayHierarchyBuilder.java
@@ -1,5 +1,6 @@
 package org.reactome.server.analysis.tools.components.filter;
 
+import org.apache.log4j.Logger;
 import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
@@ -14,18 +15,20 @@ import org.reactome.server.analysis.tools.BuilderTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 @Component
 public class PathwayHierarchyBuilder {
+    private static Logger logger = Logger.getLogger(PathwayHierarchyBuilder.class.getName());
+
     @Autowired
     private APIControllerHelper helper;
+
+    //Keeps the set of species identifiers used as main species in reactome
+    private Set<Long> mainSpecies;
 
     private Map<SpeciesNode, PathwayHierarchy> hierarchies = new HashMap<SpeciesNode, PathwayHierarchy>();
     private MapSet<Long, PathwayNode> pathwayLocation = new MapSet<Long, PathwayNode>();
@@ -33,6 +36,7 @@ public class PathwayHierarchyBuilder {
     public void build(MySQLAdaptor dba) {
         //THIS IS NEEDED HERE
         this.helper.setDba(dba);
+        this.setMainSpecies();
 
         List<Species> speciess = this.helper.getSpeciesList();
         int i = 0; int tot = speciess.size();
@@ -46,6 +50,9 @@ public class PathwayHierarchyBuilder {
             this.hierarchies.put(speciesNode, pathwayHierarchy);
             try{
                 for (GKInstance instance : this.helper.getFrontPageItems(species.getDisplayName())) {
+                    if(!this.isValidSpecies(speciesNode, instance)){
+                        continue; //Please read documentation of isValidSpecies method
+                    }
                     String stId =  this.getStableIdentifier(instance);
                     boolean hasDiagram = this.getHasDiagram(instance);
                     PathwayNode node = pathwayHierarchy.addFrontpageItem(stId, instance.getDBID(), instance.getDisplayName(), hasDiagram);
@@ -53,7 +60,7 @@ public class PathwayHierarchyBuilder {
                     if(BuilderTool.VERBOSE) {
                         System.out.print("."); // Indicates progress
                     }
-                    this.fillBranch(node, instance);
+                    this.fillBranch(speciesNode, node, instance);
                 }
             }catch (Exception e){
                 e.printStackTrace();
@@ -64,24 +71,29 @@ public class PathwayHierarchyBuilder {
         }
     }
 
-    private void fillBranch(PathwayNode node, GKInstance inst){
+    private void fillBranch(SpeciesNode speciesNode, PathwayNode node, GKInstance inst){
         try{
             if (inst.getSchemClass().isValidAttribute(ReactomeJavaConstants.hasEvent)) {
                 List<?> children = inst.getAttributeValuesList(ReactomeJavaConstants.hasEvent);
                 for (Object obj : children) {
                     GKInstance instance = (GKInstance) obj;
+                    if(!this.isValidSpecies(speciesNode, instance)){
+                        continue; //Please read documentation of isValidSpecies method
+                    }
                     if(instance.getSchemClass().isa(ReactomeJavaConstants.Pathway)){
                         String stId = this.getStableIdentifier(instance);
                         boolean hasDiagram = this.getHasDiagram(instance);
                         PathwayNode aux = node.addChild(stId, instance.getDBID(), instance.getDisplayName(), hasDiagram);
                         this.pathwayLocation.add(instance.getDBID(), aux);
-                        this.fillBranch(aux, instance);
+                        this.fillBranch(speciesNode, aux, instance);
                     }else{
-                        node.setLowerLevelPathway(true); //if the pathways has other events than pathways means it is a lower leve pathway candidate
+                        //if the pathways has other events than pathways means it is a lower level pathway candidate
+                        node.setLowerLevelPathway(true);
                     }
                 }
             }
         } catch (Exception e){
+            logger.error("Problem filling hierarchy branch for " + speciesNode.getName(), e);
             e.printStackTrace();
         }
     }
@@ -92,6 +104,13 @@ public class PathwayHierarchyBuilder {
 
     public MapSet<Long, PathwayNode> getPathwayLocation() {
         return pathwayLocation;
+    }
+
+    private GKInstance getSpecies(GKInstance instance) throws Exception {
+        if(instance.getSchemClass().isValidAttribute(ReactomeJavaConstants.species)){
+            return (GKInstance) instance.getAttributeValuesList(ReactomeJavaConstants.species).get(0);
+        }
+        return null;
     }
 
     private String getStableIdentifier(GKInstance pathway){
@@ -120,6 +139,23 @@ public class PathwayHierarchyBuilder {
         return false;
     }
 
+    /**
+     * Some oddities have been detected related to species. Those will be fixed at the curation level.
+     * In the meantime we consider a invalid species when it is different than the species of the top-level-pathway
+     * and equal to any of the other species in the main Reactome species list.
+     */
+    private boolean isValidSpecies(SpeciesNode speciesNode, GKInstance event) throws Exception {
+        GKInstance species = this.getSpecies(event);
+        if(speciesNode.getSpeciesID().equals(species.getDBID())){
+            return true;
+        }else if(this.mainSpecies.contains(species.getDBID())){
+            logger.warn(String.format("Event %d (%s) belongs to %s instead of %s",
+                    event.getDBID(), event.getDisplayName(), species.getDisplayName(), speciesNode.getName()));
+            return false;
+        }
+        return true;
+    }
+
     protected void prepareToSerialise(){
         if(BuilderTool.VERBOSE) {
             System.out.print("Setting up the resource counters for each Resource/PathwayNode");
@@ -130,6 +166,13 @@ public class PathwayHierarchyBuilder {
         }
         if(BuilderTool.VERBOSE) {
             System.out.println("\rResource counters set up successfully.");
+        }
+    }
+
+    private void setMainSpecies(){
+        this.mainSpecies = new HashSet<Long>();
+        for (Species species : this.helper.getSpeciesList()) {
+            this.mainSpecies.add(species.getDbId());
         }
     }
 }
