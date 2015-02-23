8f34221c77f8ce6ec4e170f095e64e329d2b2192
maximiliankoch <tschaka1904@gmail.com>
2015-02-23 10:42:32 +0000

    Some smaller changes

--- Report/src/main/java/org/reactome/server/statistics/util/HeaderFooterCacher.java
+++ Report/src/main/java/org/reactome/server/statistics/util/HeaderFooterCacher.java
@@ -10,7 +10,7 @@ import java.net.URL;
 /**
  * Generates the header and the footer every MINUTES defined below.
  * The header.jsp and footer.jsp are placed under jsp folder in WEB-INF
- *
+ * <p/>
  * IMPORTANT
  * ---------
  * We assume the war file runs exploded, because there is no way of writing
@@ -22,26 +22,21 @@ import java.net.URL;
  */
 public class HeaderFooterCacher extends Thread {
 
-    private static Logger logger = Logger.getLogger(HeaderFooterCacher.class.getName());
-
     private static final String TITLE_OPEM = "<title>";
     private static final String TITLE_CLOSE = "</title>";
     private static final String TITLE_REPLACE = "<title>Reactome | Pathway Analysis Report</title>";
-
     private static final String HEADER_CLOSE = "</head>";
     private static final String HEADER_CLOSE_REPLACE = "</head>";
     private static final String BODY_CLOSE = "</body>";
     private static final String BODY_CLOSE_REPLACE = "<jsp:include page=\"dependencies.jsp\"/>\n</body>";
-
     private static final String WORDPRESS = "/wordpress/";
     private static final String CONTENT = "/content/";
-
     private static final Integer MINUTES = 15;
-
+    private static Logger logger = Logger.getLogger(HeaderFooterCacher.class.getName());
     private String server;
 
     public HeaderFooterCacher(String server) {
-        if(server!=null && !server.isEmpty()) {
+        if (server != null && !server.isEmpty()) {
             this.server = server;
             logger.info("Thread to keep the header/footer updated started");
             start();
@@ -62,15 +57,15 @@ public class HeaderFooterCacher extends Thread {
         }
     }
 
-    private synchronized void writeFile(String fileName, String content){
+    private synchronized void writeFile(String fileName, String content) {
         try {
             //noinspection ConstantConditions
             String path = getClass().getClassLoader().getResource("").getPath();
             //HACK!
-            if(path.contains("WEB-INF")) {
+            if (path.contains("WEB-INF")) {
                 //When executing in a deployed war file in tomcat, the WEB-INF folder is just one bellow the classes
                 path += "../pages/";
-            }else{
+            } else {
                 //When executing in local we need to write the files in the actual resources
                 path += "../../src/main/webapp/WEB-INF/pages/";
             }
@@ -90,23 +85,23 @@ public class HeaderFooterCacher extends Thread {
             String rtn = IOUtils.toString(url.openConnection().getInputStream());
             rtn = getReplaced(rtn, TITLE_OPEM, TITLE_CLOSE, TITLE_REPLACE);
             rtn = getReplaced(rtn, HEADER_CLOSE, HEADER_CLOSE, HEADER_CLOSE_REPLACE);
-            if(!this.server.contains("localhost")) { //Dirty :(
+            if (!this.server.contains("localhost")) { //Dirty :(
                 rtn = rtn.replaceAll(WORDPRESS, this.server + WORDPRESS);
                 rtn = rtn.replaceAll(CONTENT, this.server + CONTENT);
             }
-            return  rtn;
+            return rtn;
         } catch (IOException e) {
             e.printStackTrace();
             return String.format("<span style='color:red'>%s</span>", e.getMessage());
         }
     }
 
-    private String getReplaced(String target, String open, String close, String replace){
+    private String getReplaced(String target, String open, String close, String replace) {
         try {
             String pre = target.substring(0, target.indexOf(open));
             String suf = target.substring(target.indexOf(close) + close.length(), target.length());
             return pre + replace + suf;
-        }catch (StringIndexOutOfBoundsException e){
+        } catch (StringIndexOutOfBoundsException e) {
             return target;
         }
     }
@@ -116,7 +111,7 @@ public class HeaderFooterCacher extends Thread {
             URL url = new URL(this.server + "common/footer.php");
             String rtn = IOUtils.toString(url.openConnection().getInputStream());
             rtn = rtn.replaceAll(BODY_CLOSE, BODY_CLOSE_REPLACE);
-            if(!this.server.contains("localhost")) { //Dirty :(
+            if (!this.server.contains("localhost")) { //Dirty :(
                 rtn = rtn.replaceAll(CONTENT, this.server + CONTENT);
             }
             return rtn;
