0d40e3cd88fe7558752c91c551ec9d0e4520ae29
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-10-22 14:00:45 +0100

    generifying the error handling based on AnalysisServiceException

--- Service/src/main/java/org/reactome/server/analysis/service/handler/HandlerExceptionResolverImpl.java
+++ Service/src/main/java/org/reactome/server/analysis/service/handler/HandlerExceptionResolverImpl.java
@@ -1,15 +1,17 @@
 package org.reactome.server.analysis.service.handler;
 
 import org.apache.log4j.Logger;
+import org.reactome.server.analysis.service.exception.AnalysisServiceException;
+import org.reactome.server.analysis.service.model.AnalysisError;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * By implementing the HandlerExceptionResolver we can check for different exceptions happened
@@ -18,48 +20,55 @@ import java.io.PrintWriter;
  *
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-public class HandlerExceptionResolverImpl implements org.springframework.web.servlet.HandlerExceptionResolver{
+public class HandlerExceptionResolverImpl implements HandlerExceptionResolver {
 
     private static Logger logger = Logger.getLogger(HandlerExceptionResolverImpl.class.getName());
 
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
                                          Object handler, Exception ex) {
 
-        if(ex instanceof MaxUploadSizeExceededException) {
-            response.setContentType("text/html");
+        response.setContentType("application/json");
+
+        if (ex instanceof MaxUploadSizeExceededException) {
             response.setStatus(HttpStatus.REQUEST_ENTITY_TOO_LARGE.value());
 
-            try {
-                Long maxSizeInBytes = ((MaxUploadSizeExceededException) ex).getMaxUploadSize();
-                Integer mb = (int) Math.floor(maxSizeInBytes / 1024 / 1024);
+            Long maxSizeInBytes = ((MaxUploadSizeExceededException) ex).getMaxUploadSize();
+            Integer mb = (int) Math.floor(maxSizeInBytes / 1024 / 1024);
 
-                PrintWriter out = response.getWriter();
-                out.println(
-                        "Error: " + HttpStatus.REQUEST_ENTITY_TOO_LARGE.value() + "\n " +
-                        "Message: Maximum upload size of " + mb + " MB per attachment exceeded"
-                );
-                return new ModelAndView();
+            AnalysisError error = new AnalysisError(
+                    HttpStatus.REQUEST_ENTITY_TOO_LARGE,
+                    "Maximum upload size of " + mb + " MB per attachment exceeded"
+            );
+            try {
+                response.getWriter().println(error);
             } catch (IOException e) {
                 logger.error("Error writing to output stream", e);
             }
+            return new ModelAndView();
         }
 
-        if(ex instanceof MultipartException){
-            response.setContentType("text/html");
+        if (ex instanceof MultipartException) {
             response.setStatus(HttpStatus.BAD_REQUEST.value());
-
+            AnalysisError error = new AnalysisError(HttpStatus.BAD_REQUEST, ex.getMessage());
             try {
-                PrintWriter out = response.getWriter();
-                out.println(
-                        "Error: " + HttpStatus.BAD_REQUEST.value() + "\n " +
-                        "Message: " + ex.getMessage()
-                );
-                out.println(ex.getMessage());
-
-                return new ModelAndView();
+                response.getWriter().println(error);
             } catch (IOException e) {
                 logger.error("Error writing to output stream", e);
             }
+            return new ModelAndView();
+        }
+
+        if (ex instanceof AnalysisServiceException) {
+            AnalysisServiceException ase = (AnalysisServiceException) ex;
+
+            response.setStatus(ase.getHttpStatus().value());
+            AnalysisError error = new AnalysisError(ase);
+            try {
+                response.getWriter().println(error);
+            } catch (IOException e) {
+                logger.error("Error writing to output stream", e);
+            }
+            return new ModelAndView();
         }
 
         ex.printStackTrace();
