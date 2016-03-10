590efae5ed6ba5adb3cd5f7b0bd6f9a62f5ac3c9
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-03-10 11:27:42 +0000

    aggregates entities and interactors

---
+++ Service/src/main/java/org/reactome/server/analysis/service/model/FoundElements.java
@@ -0,0 +1,57 @@
+package org.reactome.server.analysis.service.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Antonio Fabregat <fabregat@ebi.ac.uk>
+ */
+public class FoundElements {
+
+    private List<PathwayEntity> entities;
+    private List<PathwayInteractor> interactors;
+    private Set<String> resources;
+    private List<String> expNames;
+    private Integer foundEntities;
+    private Integer foundInteractors;
+
+    public FoundElements(PathwayEntities entities, PathwayInteractors interactors, List<String> expNames) {
+        this.resources = new HashSet<>();
+        this.expNames = expNames;
+        if(entities !=null){
+            this.entities = entities.getIdentifiers();
+            this.foundEntities = this.entities.size();
+            this.resources.addAll(entities.getResources());
+        }
+        if(interactors!=null){
+            this.interactors = interactors.getEntities();
+            this.foundInteractors = this.interactors.size();
+            this.resources.addAll(interactors.getResources());
+        }
+    }
+
+    public List<PathwayEntity> getEntities() {
+        return entities;
+    }
+
+    public List<PathwayInteractor> getInteractors() {
+        return interactors;
+    }
+
+    public Set<String> getResources() {
+        return resources;
+    }
+
+    public List<String> getExpNames() {
+        return expNames;
+    }
+
+    public Integer getFoundEntities() {
+        return foundEntities;
+    }
+
+    public Integer getFoundInteractors() {
+        return foundInteractors;
+    }
+}
