59bc376b6644e565b06b33f82c89c62fade10c4c
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-10-04 15:55:31 +0100

    added database info section to the API

---
+++ src/main/java/org/reactome/server/analysis/service/controller/DatabaseController.java
@@ -0,0 +1,52 @@
+package org.reactome.server.analysis.service.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.reactome.server.analysis.core.data.AnalysisData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+@Api(tags="database", description = "Database info queries", position = 0)
+public class DatabaseController {
+
+    @Autowired
+    private AnalysisData analysisData;
+
+    @ApiOperation(value = "The name of current database")
+    @ApiResponses({
+            @ApiResponse(code = 406, message = "Not acceptable according to the accept headers sent in the request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @RequestMapping(value = "/database/name", method = RequestMethod.GET, produces = "text/plain")
+    @ResponseBody
+    public String getDBName()  {
+        return analysisData.getDatabaseInfo().getName();
+    }
+
+
+    @ApiOperation(value = "The version number of current database")
+    @ApiResponses({
+            @ApiResponse(code = 406, message = "Not acceptable according to the accept headers sent in the request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
+    @RequestMapping(value = "/database/version", method = RequestMethod.GET, produces = "text/plain")
+    @ResponseBody
+    public String getDBVersion()  {
+        return "" + analysisData.getDatabaseInfo().getVersion();
+    }
+
+
+    @ApiIgnore
+    @RequestMapping(value = "/database/checksum", method = RequestMethod.GET, produces = "text/plain")
+    @ResponseBody
+    public String getChecksum() {
+        return "" + analysisData.getDatabaseInfo().getChecksum();
+    }
+}
