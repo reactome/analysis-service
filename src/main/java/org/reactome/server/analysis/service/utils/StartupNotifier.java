20292becf5e10abb62b786bdfefd59454fbf3f8e
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-11-23 13:55:58 +0000

    startup notification email configuration updated

--- src/main/java/org/reactome/server/analysis/service/utils/StartupNotifier.java
+++ src/main/java/org/reactome/server/analysis/service/utils/StartupNotifier.java
@@ -1,11 +1,13 @@
 package org.reactome.server.analysis.service.utils;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,18 +22,27 @@ public class StartupNotifier extends Thread {
     private static Logger logger = LoggerFactory.getLogger("threadLogger");
 
     private static final String PROJECT = "AnalysisService";
-    private static final String FROM = "noreply@reactome.org";
     private static final String SENDER_NAME = "Tomcat";
 
-    private final MailService sms;
-    private final String to;
+    private MailService sms;
+    private String from;
+    private String to;
 
     @Autowired
-    public StartupNotifier(MailService sms, @Value("${mail.to}") String to) {
-        super("AS-StartupNotifier");
-        this.sms = sms;
-        this.to = to;
-        start();
+    public StartupNotifier(MailService sms,
+                            @Value("${startup.notification.from}")
+                           String from,
+                            @Value("${startup.notification.to}")
+                           String to,
+                            @Value("${startup.notification}")
+                           String notify) {
+        super("DC-StartupNotifier");
+        if (Boolean.valueOf(notify) && to != null) {
+            this.sms = sms;
+            this.from = from;
+            this.to = to;
+            start();
+        }
     }
 
     @Override
@@ -41,9 +52,11 @@ public class StartupNotifier extends Thread {
 
             final String serverName = InetAddress.getLocalHost().getHostName();
             final String subject = "[" + serverName + "] " + PROJECT + " deployed " + getTimestamp();
-            final String body = PROJECT + " has been (re)deployed in [" + serverName + "]";
+            String body = PROJECT + " has been (re)deployed in [" + serverName + "]";
+            body += "\n\n List of who is logged in: \n";
+            body += getWho();
 
-            sms.send(SENDER_NAME, FROM, to, subject, body);
+            sms.send(SENDER_NAME, from, to, subject, body);
 
             logger.debug("Sent!");
         } catch (Exception e) {
@@ -55,5 +68,11 @@ public class StartupNotifier extends Thread {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return now.format(formatter);
+    }
+
+    private String getWho() throws IOException, InterruptedException {
+        ProcessBuilder pb = new ProcessBuilder("who");
+        Process process = pb.start();
+        return process.waitFor() == 0 ? IOUtils.toString(process.getInputStream()) : "<<Couldn't execute 'who' command>>";
     }
 }
