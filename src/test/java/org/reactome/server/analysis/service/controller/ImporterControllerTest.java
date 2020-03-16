dce976d61e2dea4f800b9a34bb5cc19ef72fe129
cgong <cgong@ebi.ac.uk>
2020-03-16 15:14:41 +0000

    finish test methods importController

--- src/test/java/org/reactome/server/analysis/service/controller/ImporterControllerTest.java
+++ src/test/java/org/reactome/server/analysis/service/controller/ImporterControllerTest.java
@@ -1,40 +1,60 @@
 package org.reactome.server.analysis.service.controller;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.reactome.server.analysis.AppTests;
+
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.Assert.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"file:src/test/resources/mvc-dispatcher-servlet-test.xml"})
 @WebAppConfiguration
 public class ImporterControllerTest extends AppTests {
 
+
     @Test
-    public void getPostText() {
-        // /import/
-        // post
-        // body
-        //todo input value?
+    public void getPostText() throws Exception {
+        String url = String.format("/download/%s/result.json", AppTests.token);
+        MvcResult result = mockMvcGetResult(url, MediaType.APPLICATION_JSON_UTF8_VALUE, null);
+        String response = result.getResponse().getContentAsString();
+
+        mockMvcPostResult("/import/", response);
+
     }
 
     @Test
-    public void getPostFile() {
-        // import/form
-        // post
-        // body
-        //todo input value?
+    public void getPostFile() throws Exception {
+
+        // generate a content string
+        String url = String.format("/download/%s/result.json", AppTests.token);
+        MvcResult result = mockMvcGetResult(url, MediaType.APPLICATION_JSON_UTF8_VALUE, null);
+        String response = result.getResponse().getContentAsString();
+
+        MockMultipartFile importFile = new MockMultipartFile("file", "result.json", "multipart/form-data", response.getBytes());
+
+        this.getMockMvc().perform(fileUpload("/import/form").file(importFile)).andExpect(status().isOk()).andReturn();
+
     }
 
-    @Test
-    public void getPostURL() {
-        // import/url
-        // post
-        // body
-        //todo input value?
-    }
+    /**
+     * the token needs to be included in URL and can not be access in the testing progress, skip this method
+     */
+   /* @Test
+    public void getPostURL(){
+        String input = "http://" + AppTests.HOST + ":8080" + "/download/" + AppTests.token + "/result.json";
+        mockMvcPostResult("/import/url", input);
+
+    }*/
 }
