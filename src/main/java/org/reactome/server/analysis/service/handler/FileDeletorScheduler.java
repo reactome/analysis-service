d8348ee6f8e7d3f7d86ba3db940ecb5150159f5e
cqgong <cgong@ebi.ac.uk>
2021-08-24 14:20:01 +0100

    fix the NoSuchFile exception in FileDeletorScheduler

--- src/main/java/org/reactome/server/analysis/service/handler/FileDeletorScheduler.java
+++ src/main/java/org/reactome/server/analysis/service/handler/FileDeletorScheduler.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -14,8 +13,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
+import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Component
@@ -28,21 +32,35 @@ public class FileDeletorScheduler {
     // keep the file for 15 minutes on the server then delete
     private final static int CUTOFF = 15;
 
-    @Scheduled(cron = "0 0/15 * * * * ") // every 15 minutes
+    @Scheduled(cron = "0 0/15 * * * ? ") // every 15 minutes
     public void quarterHourDeleteFiles() throws IOException {
-        Path path = Paths.get(exportedFilesTempDir);
-         //todo test below
-        // check if directory is empty
+        Path dir = Paths.get(exportedFilesTempDir);
+        if (isEmpty(dir)) return;
+        List<Path> paths = listFiles(dir);
+        paths.forEach(this::deleteOlderFiles);
+    }
+
+    // list all files from this path
+    public static List<Path> listFiles(Path path) throws IOException {
+        List<Path> result;
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk.filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+        }
+        return result;
+    }
+
+    public boolean isEmpty(Path path) throws IOException {
         if (Files.isDirectory(path)) {
-            if (Files.list(path).findAny().isPresent()) {
-                Files.walk(path)
-                        .filter(filePath -> Files.isRegularFile(filePath))
-                        .forEach(this::deleteOlderFiles);
+            try (DirectoryStream<Path> directory = Files.newDirectoryStream(path)) {
+                return !directory.iterator().hasNext();
             }
         }
+        return false;
     }
 
     private void deleteOlderFiles(Path path) {
+        if (!Files.exists(path)) return;
         FileTime fileTime = null;
         try {
             fileTime = Files.readAttributes(path, BasicFileAttributes.class).creationTime();
@@ -50,15 +68,11 @@ public class FileDeletorScheduler {
             e.printStackTrace();
         }
 
-        LocalDateTime now = LocalDateTime.now().minusMinutes(FileDeletorScheduler.CUTOFF);
-        LocalDateTime convertedFileTime = null;
-        if (fileTime != null) {
-            convertedFileTime = LocalDateTime.ofInstant(fileTime.toInstant(), ZoneId.systemDefault());
-        }
+        Instant now = Instant.now().minus(FileDeletorScheduler.CUTOFF, ChronoUnit.MINUTES);
 
-        if (convertedFileTime != null && convertedFileTime.isBefore(now)) {
+        if (fileTime != null && fileTime.toInstant().isBefore(now)) {
             try {
-                Files.delete(path);
+                Files.deleteIfExists(path);
             } catch (IOException e) {
                 e.printStackTrace();
             }
